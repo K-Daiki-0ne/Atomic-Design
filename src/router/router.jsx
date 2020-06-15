906e3f20d47f805a4d4ac2c24ceb1dee963697e4
0ne <fffdddrrrbbb@gmail.com>
2020-06-15 23:06:22 +0900

    Add: connect router

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import { Home } from '../pages/index';
 export function Router() {
   return (
     <Router>
-      <Route path exact component={}/>
+      <Route path exact component={Home}/>
     </Router>
   )
 }
