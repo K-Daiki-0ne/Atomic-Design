4c65592fb05c5ae848cc9f06007d1ede696d2fe1
0ne <fffdddrrrbbb@gmail.com>
2020-06-15 21:38:44 +0900

    Add: template route

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 export function Router() {
-  
+  return (
+    <Router>
+      <Route path exact component={}/>
+    </Router>
+  )
 }
