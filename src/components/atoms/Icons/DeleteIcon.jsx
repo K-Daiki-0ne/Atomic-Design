619c5a48e5ba1de0b19b6057392f4b56d322cec1
0ne <fffdddrrrbbb@gmail.com>
2020-06-28 12:49:03 +0900

    Division: icon component

---
+++ src/components/atoms/Icons/DeleteIcon.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+export function DeleteIcon() {
+  return (
+    <DeleteIcon />
+  );
+};
---
+++ src/components/atoms/Icons/NextIcon.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import SendIcon from '@material-ui/icons/Send';
+
+export function SendIcon(){
+  return (
+    <SendIcon />
+  );
+};
