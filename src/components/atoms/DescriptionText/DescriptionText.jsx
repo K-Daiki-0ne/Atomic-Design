09e9cdb52f3228bc5d0ab7089ee0d4ea0ffe4502
0ne <fffdddrrrbbb@gmail.com>
2020-06-28 13:19:17 +0900

    modified: description text

---
+++ src/components/atoms/DescriptionText/DescriptionText.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// AtomicDesignについての説明を記述するコンポーネント
+export function DescriptionText() {
+  return (
+    <p>
+      AtomicDesignとはUIを原子レベルまで分解してデザインしていくデザイン手法。<br />
+      AtomicDesignを使用することでUIのメンテナンス性が向上し、長期的にサービスを継続していくことが容易になります。
+      私はAtomicDesignを難しそうだと考えていましたが、今回はAtomicDesignを実践して、実際に利用してみてどう感じたかを伝えようと考えています。
+    </p>
+  );
+}
