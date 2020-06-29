9e4a7d81e0c090344ab9e73df80454b40d77c5cb
0ne <fffdddrrrbbb@gmail.com>
2020-06-29 23:33:55 +0900

    Create: base DirectoryCard component

---
+++ src/components/molecules/DirecotyCard/DirectoryCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Card } from '@material-ui/core';
+import { CardMedia } from '@material-ui/core';
+
+export function DirectoryCard() {
+  return (
+    <Card>
+      <CardMedia />
+    </Card>
+  );
+} ;
