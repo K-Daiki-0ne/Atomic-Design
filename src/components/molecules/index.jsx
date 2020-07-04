0c4c8a1b519f43fa2e5a416f43dc2298c20d206e
0ne <fffdddrrrbbb@gmail.com>
2020-07-04 20:28:09 +0900

    Add: to gather into one bundle

---
+++ src/components/molecules/index.jsx
@@ -0,0 +1,6 @@
+export * from './AuthorCard/AuthorCard';
+export * from './DateCard/DateCard';
+export * from './DirecotyCard/DirectoryCard';
+export * from './Header/Header';
+export * from './ImageCard/ImageCard';
+export * from './TextCard/TextCard';
