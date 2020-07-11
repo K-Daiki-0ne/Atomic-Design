2045f747978caa4a9052b8cf84591f261d375f73
0ne <kouguchi3456@yahoo.co.jp>
2020-07-11 22:16:08 +0900

    add: material-ui Grid

--- src/components/organisms/Header/HeaderComponent.jsx
+++ src/components/organisms/Header/HeaderComponent.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Header } from '../../molecules/index';
+import { Grid } from '@material-ui/core';
 
 export function HeaderComponent() {
-  return <Header />
+  return (
+    <Grid container spacing={3}>
+      <Grid item spacing={12}>
+        <Header />
+      </Grid>
+    </Grid>
+  )
 }
