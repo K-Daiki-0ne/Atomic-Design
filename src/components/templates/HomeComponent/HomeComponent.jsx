08e4e7c455934dce800b3657981e63e732913bb7
0ne <kouguchi3456@yahoo.co.jp>
2020-07-14 22:06:41 +0900

    create: base HomeComponent in templates

---
+++ src/components/templates/HomeComponent/HomeComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HeaderComponent } from '../../organisms/index';
+import { MainComponent } from '../../organisms/index'
+
+export function HomeComponent() {
+  return (
+    <div>
+      <HeaderComponent />
+      <MainComponent />
+    </div>
+  )
+}
