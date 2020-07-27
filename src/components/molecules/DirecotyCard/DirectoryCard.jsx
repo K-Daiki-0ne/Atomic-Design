75da60e1fd4285af5c57a06bb61bd276a14e8082
0ne <kouguchi3456@yahoo.co.jp>
2020-07-27 22:16:48 +0900

    create: directory image custom sty;e

--- src/components/molecules/DirecotyCard/DirectoryCard.jsx
+++ src/components/molecules/DirecotyCard/DirectoryCard.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Card } from '@material-ui/core';
 import { CardMedia } from '@material-ui/core';
+import { useStyle } from './style';
 
 export function DirectoryCard() {
+  const classes = useStyle();
   return (
     <Card>
       <CardMedia 
+        className={classes.image}
         component='img'
         image='/images/Directory.jpg'
         title='AtomicDesignDirectory'
---
+++ src/components/molecules/DirecotyCard/style.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyle = makeStyles({
+  image: {
+    width: '180px',
+    height: '280px',
+    marginRight: 'auto',
+    marginLeft: 'auto'
+  }
+})
