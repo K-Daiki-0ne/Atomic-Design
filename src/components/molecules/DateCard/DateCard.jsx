139dedea0a4d3f7f143978853e98fa1383a55cc6
0ne <kouguchi3456@yahoo.co.jp>
2020-07-27 23:02:33 +0900

    create: DateCard component custom style

--- src/components/molecules/DateCard/DateCard.jsx
+++ src/components/molecules/DateCard/DateCard.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Card } from '@material-ui/core';
 import { CardContent } from '@material-ui/core';
 import { DateText } from '../../atoms/index';
+import { useStyle } from './style';
 
 export function DateCard() {
+  const classes = useStyle();
   return (
-    <Card>
+    <Card className={classes.root}>
       <CardContent>
         <DateText />
       </CardContent>
