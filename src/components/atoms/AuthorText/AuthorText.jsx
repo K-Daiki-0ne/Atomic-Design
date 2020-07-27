7aa530c32a5caaf189e838977bbe60d04668bf38
0ne <kouguchi3456@yahoo.co.jp>
2020-07-27 22:37:58 +0900

    create: AuthorText Component custom style

--- src/components/atoms/AuthorText/AuthorText.jsx
+++ src/components/atoms/AuthorText/AuthorText.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
+import { useStyle } from './style';
 
 export function AuthorText() {
+  const classes = useStyle();
   return (
     <Typography
-      aligin='center'
+      className={classes.text}
       variant='h3'
     >
-      Kouguchi-Daiki
+      Kouguchi Daiki
     </Typography>
   )
 }
