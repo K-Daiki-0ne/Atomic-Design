d76cfc33c84be09e77d97798a5daedb14b2462cd
0ne <kouguchi3456@yahoo.co.jp>
2020-07-27 22:32:12 +0900

    create: AuthorCard custom style

--- src/components/molecules/AuthorCard/AuthorCard.jsx
+++ src/components/molecules/AuthorCard/AuthorCard.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Card } from '@material-ui/core';
 import { CardContent } from '@material-ui/core';
 import { AuthorText } from '../../atoms/AuthorText/AuthorText';
+import { useStyle } from './style';
 
 export function AuthorCard() {
+  const classes = useStyle();
   return (
-    <Card>
+    <Card className={classes.root}>
       <CardContent>
         <AuthorText />
       </CardContent>
