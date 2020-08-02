5d295e182062c838211959e7006c2078b3d4850f
_ 0ne <fffdddrrrbbb@gmail.com>
2020-08-02 22:44:55 +0900

    add: together component

--- src/components/atoms/index.jsx
+++ src/components/atoms/index.jsx
@@ -3,3 +3,5 @@ export * from './DateText/DateText';
 export * from './DescriptionHeader/DescriptionHeader';
 export * from './DescriptionText/DescriptionText';
 export * from './HeaderText/HeaderText';
+export * from './ImageTitle/ImageTitle';
+export * from './DirectoryTitle/DirectoryTitle';
