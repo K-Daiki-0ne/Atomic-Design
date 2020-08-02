bb709a1806f2ae03c80cf0c96266c406a48b4845
_ 0ne <fffdddrrrbbb@gmail.com>
2020-08-02 15:12:08 +0900

    add: connect style

--- src/components/atoms/DescriptionHeader/DescriptionHeader.jsx
+++ src/components/atoms/DescriptionHeader/DescriptionHeader.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
+import { useStyle } from './style';
 
 // AtomicDesignを説明するためのタイトル
 export function DescriptionHeader() {
+  const classes = useStyle();
   return (
     <Typography
       variant='h6'
+      className={classes.text}
     >
       AtomicDesignについて
     </Typography>
