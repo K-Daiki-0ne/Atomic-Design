ad0a64e18733e2ce6632a43a30d035a40be490c3
_ 0ne <fffdddrrrbbb@gmail.com>
2020-08-02 15:47:17 +0900

    fix: Header design

--- src/components/molecules/Header/Header.jsx
+++ src/components/molecules/Header/Header.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { AppBar } from '@material-ui/core';
 import { Toolbar } from '@material-ui/core';
 import { HeaderText } from '../../atoms/index';
+import { useStyle } from './style';
 
 export function Header() {
+  const classes = useStyle();
   return (
-    <AppBar>
+    <AppBar className={classes.root}>
       <Toolbar>
         <HeaderText />
       </Toolbar>
