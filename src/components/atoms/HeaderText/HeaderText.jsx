2bc44be3e8aeef93a06700cc37b64220efda521f
_ 0ne <fffdddrrrbbb@gmail.com>
2020-08-02 15:41:18 +0900

    create: HeaderText custom style

--- src/components/atoms/HeaderText/HeaderText.jsx
+++ src/components/atoms/HeaderText/HeaderText.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
+import { useStyle } from './style';
 
 export function HeaderText() {
+  const classes = useStyle();
   return (
     <Typography
-      variant='h6'
+      variant='h1'
+      className={classes.text}
     >
       AtomicDesignについての学習
     </Typography>
