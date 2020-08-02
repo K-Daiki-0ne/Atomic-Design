83136cfb8113b5cd138a990f499ba5bd8f62f6e9
_ 0ne <fffdddrrrbbb@gmail.com>
2020-08-02 15:02:49 +0900

    create: DescriptionCard custom design

---
+++ src/components/molecules/DescriptionCard/style.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyle = makeStyles({
+  root: {
+    backgroundColor: 'white'
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: '24px',
+    fontWeight: '400',
+    color: '#000066'
+  }
+})
